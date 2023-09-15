6a774a9a1b164c1273caaeaf37c3b9daf59329d3
Yehia-Ahmed <yehia790200@gmail.com>
2023-09-15 11:48:51 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div [class]="loginForm">
+      <p [class]="alertStyle">* Email or Passward isn't valid</p>
+      <h3>Login</h3>
+      <label class="form_control">
+        username
+        <input
+          type="text"
+          name="username"
+          [ngModel]="userName"
+          (ngModelChange)="userName = $event"
+        />
+      </label>
+      <label class="form_control">
+        Passward
+        <input
+          type="password"
+          name="password"
+          [ngModel]="password"
+          (ngModelChange)="password = $event"
+        />
+      </label>
+      <button (click)="onSubmitHandler()">submit</button>
+    </div>
+
+    <div [class]="welcomePage">
+      <h1>Welcome Back!</h1>
+      <button (click)="logoutHandler()">Logout</button>
+    </div>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  showAlertMassage: boolean = false;
+  loginForm: string = 'form_container';
+  welcomePage: string = 'welcome_page hidden';
+  alertStyle: string = 'alert_Massage hidden';
+  userName: string = '';
+  password: string = '';
+  displayUserName() {}
+  resetInput() {
+    this.userName = '';
+    this.password = '';
+  }
+  loginHandler() {
+    this.loginForm = 'form_container_hidden';
+    this.welcomePage = 'welcome_page';
+  }
+  logoutHandler() {
+    this.loginForm = 'form_container';
+    this.welcomePage = 'welcome_page hidden';
+  }
+  showAlertMassageHandler() {
+    if (!this.showAlertMassage) {
+      this.alertStyle = 'alert_Massage';
+    }
+  }
+  onSubmitHandler() {
+    // e.preventDefault();
+    if (this.userName === 'admin' && this.password === '1111') {
+      console.log(true);
+      this.resetInput();
+      this.loginHandler();
+    } else {
+      this.resetInput();
+      this.showAlertMassageHandler();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
